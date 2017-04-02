e5d36773dee85d5843dedc82a28b9a0b044a0806
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-04-02 16:49:28 +0900

    Add argument and attribute client

--- lib/mikoshi/plan.rb
+++ lib/mikoshi/plan.rb
@@ -5,12 +5,13 @@ require 'yaml'
 module Mikoshi
   class Plan
     class Base
-      attr_reader :data
+      attr_reader :data, :client
 
-      def initialize(yaml_path: nil)
+      def initialize(yaml_path: nil, client: nil)
         raise ArgumentError, 'Yaml file path is required.' if yaml_path.nil?
 
         @data = Hashie::Mash.new(YAML.safe_load(ERB.new(File.new(yaml_path).read).result))
+        @client = client
       end
     end
   end
