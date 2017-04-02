5817a7a2465e96c96dcb269a97e10d674490e0eb
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-04-02 17:01:44 +0900

    Create Mikoshi::Plan::TaskDefinition for task_definition

--- lib/mikoshi.rb
+++ lib/mikoshi.rb
@@ -1,6 +1,7 @@
 require 'mikoshi/version'
 require 'mikoshi/manager'
 require 'mikoshi/plan'
+require 'mikoshi/plan/task_definition'
 
 module Mikoshi
   # Your code goes here...
---
+++ lib/mikoshi/plan/task_definition.rb
@@ -0,0 +1,11 @@
+require 'mikoshi/plan'
+
+module Mikoshi
+  class Plan
+    class TaskDefinition < Base
+      def initialize(yaml_path: nil, client: nil)
+        super
+      end
+    end
+  end
+end
