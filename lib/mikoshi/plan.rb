e31bd72113bac701fc4d8c2f10c4b379f2662550
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-04-02 16:48:11 +0900

    Nest class Plan::Base

--- lib/mikoshi/plan.rb
+++ lib/mikoshi/plan.rb
@@ -4,12 +4,14 @@ require 'yaml'
 
 module Mikoshi
   class Plan
-    attr_reader :data
+    class Base
+      attr_reader :data
 
-    def initialize(yaml_path: nil)
-      raise ArgumentError, 'Yaml file path is required.' if yaml_path.nil?
+      def initialize(yaml_path: nil)
+        raise ArgumentError, 'Yaml file path is required.' if yaml_path.nil?
 
-      @data = Hashie::Mash.new(YAML.safe_load(ERB.new(File.new(yaml_path).read).result))
+        @data = Hashie::Mash.new(YAML.safe_load(ERB.new(File.new(yaml_path).read).result))
+      end
     end
   end
 end
