9b6c4625b44fc6245faf02542374690622b1f95f
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-04-06 17:38:21 +0900

    Raise error when client is nil

--- lib/mikoshi/plan.rb
+++ lib/mikoshi/plan.rb
@@ -9,6 +9,7 @@ module Mikoshi
 
       def initialize(yaml_path: nil, client: nil)
         raise ArgumentError, 'Yaml file path is required.' if yaml_path.nil?
+        raise ArgumentError, 'client is required.' if client.nil?
 
         @data = YAML.safe_load(ERB.new(File.new(yaml_path).read).result).symbolize_keys
         @client = client
