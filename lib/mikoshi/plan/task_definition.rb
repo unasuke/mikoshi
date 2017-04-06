1828970d1e4ac6023d7b40672fc986c3b494d714
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-04-06 17:47:10 +0900

    Register task definition and store revision to env

--- lib/mikoshi/plan/task_definition.rb
+++ lib/mikoshi/plan/task_definition.rb
@@ -6,6 +6,12 @@ module Mikoshi
       def initialize(yaml_path: nil, client: nil)
         super
       end
+
+      def register_task_definition
+        resp = @client.register_task_definition(@data)
+        ENV['TASK_REVISION'] = resp.tas_definition.revision.to_s
+        resp
+      end
     end
   end
 end
