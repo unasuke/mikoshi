e5ac9fb9b034228fee3abaca497ff181783c5c03
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-04-06 20:44:02 +0900

    Create non-cli func aws_client

--- lib/mikoshi/cli.rb
+++ lib/mikoshi/cli.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'aws-sdk'
 
 module Mikoshi
   class Cli < Thor
@@ -10,6 +11,18 @@ module Mikoshi
     desc 'deploy TASK_NAME', 'Deploy task definition'
     def deploy(task_name)
       puts task_name
+    end
+
+    no_tasks do
+      def aws_client
+        if ENV['AWS_REGION'].nil? && options[:region].nil?
+          raise ArgumentError, 'aws region is required'
+        end
+        region = ENV['AWS_REGION']
+        region = options[:region] unless options[:region].nil?
+
+        Aws::ECS::Client.new(region: region)
+      end
     end
   end
 end
