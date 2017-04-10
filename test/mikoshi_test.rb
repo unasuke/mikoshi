430e56b0d26dec9cf989d3d1c60577e3893385e2
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-04-10 18:53:19 +0900

    Remove test it always fail

--- test/mikoshi_test.rb
+++ test/mikoshi_test.rb
@@ -4,8 +4,4 @@ class MikoshiTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Mikoshi::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
