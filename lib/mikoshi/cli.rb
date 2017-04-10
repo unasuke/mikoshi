b02458ddadaa3654b5805a2a351e723aaabfc1c7
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-04-10 17:19:17 +0900

    Create cli command update_service

--- lib/mikoshi/cli.rb
+++ lib/mikoshi/cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'aws-sdk'
+require 'timeout'
 require 'mikoshi/plan'
 require 'mikoshi/plan/task_definition'
 require 'mikoshi/plan/service'
@@ -9,6 +10,8 @@ module Mikoshi
     TASK_DEFINITION_PATH = 'task_definitions'.freeze
     SERVICE_PATH = 'services'.freeze
     PLAN_EXT = '.yml.erb'.freeze
+    FETCH_INTERVAL = 10
+    DEPLOY_TIMEOUT = 300
 
     class_option :region, type: :string, desc: 'aws region'
 
@@ -21,6 +24,29 @@ module Mikoshi
       puts "Update task definition: #{task_name}"
       task.register_task_definition
       puts "Done update task definition: #{task_name} revison: #{ENV['TASK_REVISION']}"
+    end
+
+    desc 'update_service SERVICE_NAME', 'Update service'
+    def update_service(service_name)
+      service = ::Mikoshi::Plan::Service.new(
+        yaml_path:  File.join(SERVICE_PATH, service_name + PLAN_EXT),
+        client:     aws_client
+      )
+      puts "Update service : #{service_name}"
+      service.deploy_service
+      begin
+        Timeout.timeout(DEPLOY_TIMEOUT) do
+          loop do
+            puts "Waiting for #{FETCH_INTERVAL} sec..."
+            sleep FETCH_INTERVAL
+            break if service.deployed?
+          end
+        end
+      rescue Timeout::Error
+        puts "Deploy failed by timeout(#{DEPLOY_TIMEOUT} sec)"
+      end
+
+      puts "Deploy success"
     end
 
     desc 'deploy TASK_NAME', 'Deploy task definition'
